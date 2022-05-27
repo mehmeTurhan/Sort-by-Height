1e74c7897403ac013b7e122d0198d16b5d3e3e51
Mehmet Turhan <97054495+mehmeTurhan@users.noreply.github.com>
2022-05-26 20:09:57 -0400

    Update sortByHeight.java

--- sortByHeight.java
+++ sortByHeight.java
@@ -3,8 +3,7 @@ int[] solution(int[] a) {
     int min=0;
     for(int i=0; i<a.length; i++){
         for(int j=i+1; j<b.length; j++){
-            if(a[i]!=-1){
-                if(b[j]!=-1){
+            if(a[i]!=-1 && b[j]!=-1){
                     min = a[i];
                     if(min>b[j]){
                         a[i]=b[j];
